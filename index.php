eb98c624691c0faffa7157eda6a5f9dba3caca8e
HT <cs112092@gmail.com>
2016-11-28 21:58:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>個人首頁</title>
+    <link rel="stylesheet" type="text/css" href="animate.css">
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js 讓 IE8 支援 HTML5 元素與媒體查詢 -->
+    <!-- 警告：Respond.js 無法在 file:// 協定下運作 -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+          html {
+              height: 100%;
+          }
+
+          body {
+              background-image: url('bg.jpg');
+              background-repeat: no-repeat;
+              background-attachment: fixed;
+              background-position: center;
+              background-size: cover;
+          }
+          #font-w {
+            color:#FFF
+          }
+      </style>
+  </head>
+  <body>
+<div class="container">
+  <div class="row" style=" text-align:center;margin:30px 0px" >
+    <div class="page-header animated fadeInDown" id="font-w">
+      <h1><b>個人首頁</b> <small>Introduction</small></h1>
+    </div>
+    <div class="row">
+      <div class="col-md-offset-4 col-sm-6 col-md-4">
+        <div class="thumbnail animated zoomIn"  style=" text-align:center">
+          <img src="cat.jpg" alt="...">
+          <div class="caption"><b>
+            <h3>個人簡介</h3>
+            <p>資工二乙</p>
+            <p>陳嘉祥</p>
+            <p> <a href="mailto:k40418208@gcloud.csu.edu.tw">k40418208@gcloud.csu.edu.tw</a></p></b>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
+  </div>
+
+</div>
+
+
+    <!-- jQuery (Bootstrap 所有外掛均需要使用) -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- 依需要參考已編譯外掛版本（如下），或各自獨立的外掛版本 -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
